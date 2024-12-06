fc45e33fe407378259fd4b915f2963c7a0fdd2a8
SeverinRoman <109161620+SeverinRoman@users.noreply.github.com>
2024-12-06 03:42:10 +0300

    - add input

--- Silent/Intermediate/Build/Win64/x64/SilentEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/GitHub/silent/Silent/Intermediate/Build/Win64/x64/SilentEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Silent/Intermediate/Build/Win64/x64/SilentEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/GitHub/silent/Silent/Intermediate/Build/Win64/x64/SilentEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
